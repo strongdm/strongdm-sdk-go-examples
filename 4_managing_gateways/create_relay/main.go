f36f3d7acc0f7f4508b50af3fd00977a995eaa21
Craig Jones <craig@strongdm.com>
2020-07-15 15:35:00 -0500

    4_managing_gateways: added create_relay example

---
+++ 4_managing_gateways/create_relay/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	sdm "github.com/strongdm/strongdm-sdk-go"
+)
+
+func main() {
+	//	Load the SDM API keys from the environment.
+	//	If these values are not set in your environment,
+	//	please follow the documentation here:
+	//	https://www.strongdm.com/docs/admin-guide/api-credentials/
+	accessKey := os.Getenv("SDM_API_ACCESS_KEY")
+	secretKey := os.Getenv("SDM_API_SECRET_KEY")
+	if accessKey == "" || secretKey == "" {
+		log.Fatal("SDM_API_ACCESS_KEY and SDM_API_SECRET_KEY must be provided")
+	}
+
+	// Create the client
+	client, err := sdm.New(
+		accessKey,
+		secretKey,
+		sdm.WithHost("api.strongdmdev.com:443"),
+	)
+	if err != nil {
+		log.Fatalf("could not create client: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	relay := &sdm.Relay{
+		Name: "example-relay",
+	}
+
+	relayResponse, err := client.Nodes().Create(ctx, relay)
+	if err != nil {
+		log.Fatalf("Could not create relay: %v", err)
+	}
+
+	relayID := relayResponse.Node.GetID()
+
+	log.Printf("Successfully created relay.\n\tID: %v\n", relayID)
+}
