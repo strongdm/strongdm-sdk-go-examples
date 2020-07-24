c408870ad6f0036a1c4d61cbcc7612bcbb014716
Craig Jones <craig@strongdm.com>
2020-07-24 12:41:24 -0500

    1_managing_resources: add delete_resource example

---
+++ 1_managing_resources/delete_resource/main.go
@@ -0,0 +1,79 @@
+// Copyright 2020 StrongDM Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	sdm "github.com/strongdm/strongdm-sdk-go"
+)
+
+func main() {
+	//	Load the SDM API keys from the environment.
+	//	If these values are not set in your environment,
+	//	please follow the documentation here:
+	//	https://www.strongdm.com/docs/admin-guide/api-credentials/
+	accessKey := os.Getenv("SDM_API_ACCESS_KEY")
+	secretKey := os.Getenv("SDM_API_SECRET_KEY")
+	if accessKey == "" || secretKey == "" {
+		log.Fatal("SDM_API_ACCESS_KEY and SDM_API_SECRET_KEY must be provided")
+	}
+
+	// Create the client
+	client, err := sdm.New(
+		accessKey,
+		secretKey,
+		sdm.WithHost("api.strongdmdev.com:443"),
+	)
+	if err != nil {
+		log.Fatalf("could not create client: %v", err)
+	}
+
+	// Define the datasource
+	examplePostgresDatasource := &sdm.Postgres{
+		Name:         "Example Postgres Datasource",
+		Hostname:     "example.strongdm.com",
+		Port:         5432,
+		Username:     "example",
+		Password:     "example",
+		Database:     "example",
+		PortOverride: 19999,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	// Create  the datasource
+	createResponse, err := client.Resources().Create(ctx, examplePostgresDatasource)
+	if err != nil {
+		log.Fatalf("Could not create Postgres datasource: %v", err)
+	}
+
+	id := createResponse.Resource.GetID()
+	name := createResponse.Resource.GetName()
+
+	log.Printf("Successfully created Postgres datasource.\n\tName: %v\n\tID: %v\n", name, id)
+
+	// Delete the resource
+	_, err = client.Resources().Delete(ctx, id)
+	if err != nil {
+		log.Fatalf("Could not delete Postgres datasource: %v", err)
+	}
+
+	log.Println("Successfully delete Postgres datasource.")
+}
