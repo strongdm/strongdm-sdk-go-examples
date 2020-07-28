c721b1812cdafe559e403e30056c53c60e968e4e
Phil Hassey <phil@galcon.com>
2020-07-28 14:23:13 -0600

    4_managing_gateways: add delete gateway example

---
+++ 4_managing_gateways/delete_gateway/main.go
@@ -0,0 +1,71 @@
+// Copyright 2020 StrongDM Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	sdm "github.com/strongdm/strongdm-sdk-go"
+)
+
+func main() {
+	//	Load the SDM API keys from the environment.
+	//	If these values are not set in your environment,
+	//	please follow the documentation here:
+	//	https://www.strongdm.com/docs/admin-guide/api-credentials/
+	accessKey := os.Getenv("SDM_API_ACCESS_KEY")
+	secretKey := os.Getenv("SDM_API_SECRET_KEY")
+	if accessKey == "" || secretKey == "" {
+		log.Fatal("SDM_API_ACCESS_KEY and SDM_API_SECRET_KEY must be provided")
+	}
+
+	// Create the client
+	client, err := sdm.New(
+		accessKey,
+		secretKey,
+	)
+	if err != nil {
+		log.Fatalf("could not create client: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	create := &sdm.Gateway{
+		Name:          "example-gateway",
+		ListenAddress: "gateway.example.com:5555",
+	}
+
+	createResponse, err := client.Nodes().Create(ctx, create)
+	if err != nil {
+		log.Fatalf("Could not create gateway: %v", err)
+	}
+
+	id := createResponse.Node.GetID()
+	token := createResponse.Token
+	log.Println("Successfully created gateway.")
+	log.Println("\tID:", id)
+	log.Println("\tToken:", token)
+
+	// Delete the gateway
+	_, err = client.Nodes().Delete(ctx, id)
+	if err != nil {
+		log.Fatalf("Could not delete gateway: %v", err)
+	}
+	log.Println("Successfully deleted gateway.")
+}
