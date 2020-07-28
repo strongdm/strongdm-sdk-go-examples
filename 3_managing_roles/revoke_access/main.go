3cd8659aa6b93cc86be614325eab82f4015ae287
Phil Hassey <phil@galcon.com>
2020-07-28 12:33:58 -0600

    3_managing_roles: add revoke access example

---
+++ 3_managing_roles/revoke_access/main.go
@@ -0,0 +1,140 @@
+// Copyright 2020 StrongDM Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	sdm "github.com/strongdm/strongdm-sdk-go"
+)
+
+func main() {
+	//	Load the SDM API keys from the environment.
+	//	If these values are not set in your environment,
+	//	please follow the documentation here:
+	//	https://www.strongdm.com/docs/admin-guide/api-credentials/
+	accessKey := os.Getenv("SDM_API_ACCESS_KEY")
+	secretKey := os.Getenv("SDM_API_SECRET_KEY")
+	if accessKey == "" || secretKey == "" {
+		log.Fatal("SDM_API_ACCESS_KEY and SDM_API_SECRET_KEY must be provided")
+	}
+
+	// Create the client
+	client, err := sdm.New(
+		accessKey,
+		secretKey,
+	)
+	if err != nil {
+		log.Fatalf("could not create client: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	// Create a datasource
+	examplePostgresDatasource := &sdm.Postgres{
+		Name:             "Example Postgres Datasource",
+		Hostname:         "example.strongdm.com",
+		Port:             5432,
+		Username:         "example",
+		Password:         "example",
+		Database:         "example",
+		OverrideDatabase: true,
+		PortOverride:     19999,
+	}
+
+	resourceResponse, err := client.Resources().Create(ctx, examplePostgresDatasource)
+	if err != nil {
+		log.Fatalf("Could not create Postgres datasource: %v", err)
+	}
+
+	resourceID := resourceResponse.Resource.GetID()
+
+	log.Printf("Successfully created Postgres datasource.\n\tID: %v\n", resourceID)
+
+	// Create a role
+	role := &sdm.Role{
+		Name: "example role",
+	}
+
+	roleResponse, err := client.Roles().Create(ctx, role)
+	if err != nil {
+		log.Fatalf("Could not create role: %v", err)
+	}
+
+	roleID := roleResponse.Role.ID
+	log.Printf("Successfully created role.\n\tID: %v\n", roleID)
+
+	// Create a role grant
+	grant := &sdm.RoleGrant{
+		ResourceID: resourceID,
+		RoleID:     roleID,
+	}
+
+	grantResponse, err := client.RoleGrants().Create(ctx, grant)
+	if err != nil {
+		log.Fatalf("Could not create account grant: %v", err)
+	}
+
+	grantID := grantResponse.RoleGrant.ID
+
+	log.Printf("Successfully created role grant.\n\tID: %v\n", grantID)
+
+	// Create a user
+	user := &sdm.User{
+		Email:     "example@example.com",
+		FirstName: "example",
+		LastName:  "example",
+	}
+
+	accountResponse, err := client.Accounts().Create(ctx, user)
+	if err != nil {
+		log.Fatalf("Could not create user: %v", err)
+	}
+
+	accountID := accountResponse.Account.GetID()
+	log.Printf("Successfully created user.\n\tID: %v\n", accountID)
+
+	// Assign account to role
+	attachment := &sdm.AccountAttachment{
+		AccountID: accountID,
+		RoleID:    roleID,
+	}
+
+	attachmentResponse, err := client.AccountAttachments().Create(ctx, attachment)
+	if err != nil {
+		log.Fatalf("Could not create account attachment: %v", err)
+	}
+
+	attachmentID := attachmentResponse.AccountAttachment.ID
+	log.Printf("Successfully created account attachment.\n\tID: %v\n", attachmentID)
+
+	// Detatch user from role
+	_, err = client.AccountAttachments().Delete(ctx, attachmentID)
+	if err != nil {
+		log.Fatalf("Could not delete account attachment: %v", err)
+	}
+	log.Println("Successfully deleted account attachment.")
+
+	// Delete grant from role
+	_, err = client.RoleGrants().Delete(ctx, grantID)
+	if err != nil {
+		log.Fatalf("Could not delete role grant: %v", err)
+	}
+	log.Println("Successfully deleted role grant.")
+}
