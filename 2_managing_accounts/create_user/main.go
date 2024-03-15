626a53ab8ad18729fa688b9be4073bc8214b375f
Jess Schmidt <schmidt.jessh@gmail.com>
2024-03-15 12:08:49 -0500

    Add examples of creating users at a specific permission level, suspending users, and updating a user's permission level.

--- 2_managing_accounts/create_user/main.go
+++ 2_managing_accounts/create_user/main.go
@@ -4,14 +4,13 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
 package main
 
 import (
@@ -21,7 +20,7 @@ import (
 	"os"
 	"time"
 
-	sdm "github.com/strongdm/strongdm-sdk-go/v2"
+	sdm "github.com/strongdm/strongdm-sdk-go/v7"
 )
 
 func main() {
@@ -47,9 +46,10 @@ func main() {
 
 	// Create a User
 	user := &sdm.User{
-		Email:     "create-user@example.com",
-		FirstName: "example",
-		LastName:  "example",
+		Email:           "create-user@example.com",
+		FirstName:       "example",
+		LastName:        "example",
+		PermissionLevel: sdm.PermissionLevelTeamLeader,
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
