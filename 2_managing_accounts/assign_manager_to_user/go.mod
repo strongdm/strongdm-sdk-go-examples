14e8b08e67bc9252b2bad8c675397194bb65ade1
johnnyhchen <johnnychenv@gmail.com>
2025-05-08 18:50:00 -0700

    2_managing_accounts/assign_manager_to_user/main.go: add example for manager ID and SCIM metadata

---
+++ 2_managing_accounts/assign_manager_to_user/go.mod
@@ -0,0 +1,5 @@
+module github.com/strongdm/strongdm-sdk-go-examples/2_managing_accounts/assign_manager_to_user
+
+go 1.14
+
+require github.com/strongdm/strongdm-sdk-go/v14 v14.10.0
---
+++ 2_managing_accounts/assign_manager_to_user/main.go
@@ -0,0 +1,120 @@
+// Copyright 2020 StrongDM Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	sdm "github.com/strongdm/strongdm-sdk-go/v14"
+)
+
+func main() {
+	log.SetFlags(0)
+	//	Load the SDM API keys from the environment.
+	//	If these values are not set in your environment,
+	//	please follow the documentation here:
+	//	https://www.strongdm.com/docs/api/api-keys/
+	accessKey := os.Getenv("SDM_API_ACCESS_KEY")
+	secretKey := os.Getenv("SDM_API_SECRET_KEY")
+	if accessKey == "" || secretKey == "" {
+		log.Fatal("SDM_API_ACCESS_KEY and SDM_API_SECRET_KEY must be provided")
+	}
+
+	// Create the client
+	client, err := sdm.New(accessKey, secretKey, sdm.WithHost("localhost:8888"), sdm.WithInsecure())
+	if err != nil {
+		log.Fatal("failed to create strongDM client:", err)
+	}
+
+	// Create a User
+	user := &sdm.User{
+		Email:           "create-user@example.com",
+		FirstName:       "example",
+		LastName:        "example",
+		PermissionLevel: sdm.PermissionLevelTeamLeader,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	createResponse, err := client.Accounts().Create(ctx, user)
+	if err != nil {
+		log.Fatalf("Could not create user: %v", err)
+	}
+
+	id := createResponse.Account.GetID()
+
+	fmt.Println("Successfully created user.")
+	fmt.Println("\tID:", id)
+
+	// Assign user as manager for another user
+	user2 := &sdm.User{
+		Email:     "create-user2@example.com",
+		FirstName: "example2",
+		LastName:  "example2",
+		ManagerID: id,
+	}
+
+	ctx, cancel = context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	createResponse, err = client.Accounts().Create(ctx, user2)
+	if err != nil {
+		log.Fatalf("Could not create user: %v", err)
+	}
+
+	createdUser := createResponse.Account.(*sdm.User)
+
+	fmt.Println("Successfully created user.")
+	fmt.Println("\tID:", createdUser.ID)
+	fmt.Println("\tManagerID:", createdUser.ManagerID)
+
+	// Get Resolved Manager ID of user and SCIM metadata if present.
+	// Resolved Manager ID will be set to the manager ID of the user, if present,
+	// and will be resolved from manager information from SCIM metadata otherwise.
+	// If no manager information can be resolved from SCIM metadata and manager ID is not set,
+	// Resolved Manager ID will have no value.
+	getResponse, err := client.Accounts().Get(ctx, createdUser.ID)
+	if err != nil {
+		log.Fatalf("get user failed: %v", err)
+	}
+	gotUser := getResponse.Account.(*sdm.User)
+
+	fmt.Println("Successfully got user.")
+	fmt.Println("\tID:", gotUser.ID)
+	fmt.Println("\tManagerID:", gotUser.ManagerID)
+	fmt.Println("\tResolvedManagerID:", gotUser.ResolvedManagerID)
+	fmt.Println("\tSCIM Metadata:", gotUser.SCIM)
+
+	// Update ManagerID on user
+	gotUser.ManagerID = ""
+
+	ctx, cancel = context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	updateResponse, err := client.Accounts().Update(ctx, gotUser)
+	if err != nil {
+		log.Fatalf("Could not update user: %v", err)
+	}
+
+	updatedUser := updateResponse.Account.(*sdm.User)
+
+	fmt.Println("Successfully updated user.")
+	fmt.Println("\tID:", updatedUser.ID)
+	fmt.Println("\tManagerID:", updatedUser.ManagerID)
+}
